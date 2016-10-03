8288905c6e7cb698cdbdb3169cdca2b9d7ae12d4
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-10-03 21:13:16 +0700

    refactor function

--- ratelimit.js
+++ ratelimit.js
@@ -21,57 +21,50 @@ module.exports.rateLimit = (options) => {
   var limit = options.limit || 1000
   var resetTime = reset.split(' ')
   return (req, res, next) => {
-
     function setKey (opt) {
       storage.put(opt, (err) => {
         if (err) console.log(err)
+        req.rateLimitObj = opt.objectKey
+        res.setHeader('X-RateLimit-Limit', opt.objectKey.limit)
+        res.setHeader('X-RateLimit-Remaining', opt.objectKey.remaining)
+        res.setHeader('X-RateLimit-Reset', opt.objectKey.reset)
+        next()
       })
-    }
-
-    function setHeaders (opt) {
-      res.setHeader('X-RateLimit-Limit', opt.limit)
-      res.setHeader('X-RateLimit-Remaining', opt.remaining)
-      res.setHeader('X-RateLimit-Reset', opt.reset)
-    }
-
-    function addNewKey (requestKey) {
-      var objectKey = {
-        limit: limit,
-        reset: moment().add(resetTime[0], resetTime[1]),
-        remaining: limit-1
-      }
-      setKey({
-        requestKey: requestKey,
-        objectKey: objectKey
-      })
-      setHeaders(objectKey)
     }
 
     var requestKey = keyGenerator(req, res)
     console.log('requestor', requestKey)
     storage.get(requestKey, (err, value) => {
-      console.log(err, value)
       if (err || !value) {
-        addNewKey(requestKey)
-        next()
-      }
-      else {
+        setKey({
+          requestKey: requestKey,
+          objectKey: {
+            limit: limit,
+            reset: moment().add(resetTime[0], resetTime[1]),
+            remaining: limit-1
+          }
+        })
+      } else {
         var limitObj = JSON.parse(value)
         if (moment(limitObj.reset) < moment()){
-          addNewKey(requestKey)
-          next()
+          setKey({
+            requestKey: requestKey,
+            objectKey: {
+              limit: limit,
+              reset: moment().add(resetTime[0], resetTime[1]),
+              remaining: limit-1
+            }
+          })
         } else if (limitObj.remaining === 0) {
           (options.limitCallback) 
             ? options.limitCallback(req, res, next, extend({}, limitObj, {ip: ip, url: url})) 
             : res.status(429).send('You shall not pass!')
         } else {
           var newLimit = extend({}, limitObj, {remaining: limitObj.remaining-1})
-          setHeaders(newLimit)
           setKey({
             requestKey: requestKey,
             objectKey: newLimit
           })
-          next()
         }
       }
     })
